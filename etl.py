319ccee8dfd04f89f01869e7e7e1a11f581a8951
Xuan Hai <dxhai311@gmail.com>
2026-06-13 16:59:26 +0700

    Implement ETL pipeline for core Olist tables

--- etl.py
+++ etl.py
@@ -12,7 +12,42 @@ def extract(file_name : str) -> pd.DataFrame:
 
 	return df 
 
-def transform(df : pd.DataFrame) -> pd.DataFrame:
+def transform(df : pd.DataFrame, table_name) -> pd.DataFrame:
+	if table_name == 'orders':
+		return transform_orders(df)
+	elif table_name == 'products':
+		return transform_products(df)
+	elif table_name == 'customers':
+		return transform_basic(df, 'customer_id')
+	elif table_name == 'sellers':
+		return transform_basic(df, 'seller_id')
+	else:
+		return df
+
+def transform_products(df : pd.DataFrame) -> pd.DataFrame:
+
+	#B1: Drop duplicate theo primary key
+	df = df.drop_duplicates(subset = 'product_id')
+
+	#B2: Xóa các đơn thiếu thông tin 
+	df = df.dropna(subset = [
+		'product_category_name',
+		'product_name_lenght',
+		'product_description_lenght',
+		'product_photos_qty'
+	])
+
+	return df
+
+def transform_basic(df : pd.DataFrame, primary_key : str) -> pd.DataFrame:
+	"""Transform basic primary key"""
+
+	#B1: Drop duplicate theo primary key
+	df = df.drop_duplicates(subset = primary_key)
+
+	return df
+
+def transform_orders(df : pd.DataFrame) -> pd.DataFrame:
 	"""
 	Transfrom delivered orders only.
 	"""
@@ -25,7 +60,11 @@ def transform(df : pd.DataFrame) -> pd.DataFrame:
 	df = df.drop_duplicates(subset = ['order_id'])
 
 	#B3: Xóa các cột NULL vì đã lọc delivered nên không thể NULL
-	df = df.dropna(subset = ['order_approved_at', 'order_delivered_carrier_date', 'order_delivered_customer_date'])
+	df = df.dropna(subset = [
+		'order_approved_at',
+		'order_delivered_carrier_date',
+		'order_delivered_customer_date'
+		])
 
 	#B4: Str convert datetime
 	date_cols = [
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from etl import extract, transform, load
+from etl import (
+	extract,
+ 	transform,
+ 	load
+)
 from db import get_engine
 
+TABLES = [
+	('olist_orders_dataset.csv', 'orders'),
+	('olist_customers_dataset.csv', 'customers'),
+	('olist_sellers_dataset.csv', 'sellers'),
+	('olist_products_dataset.csv', 'products')
+]
+
 def run_pipeline():
-
-	df = extract(
-		"olist_orders_dataset.csv"
-	)
-
-	df = transform(df)
-
 	engine = get_engine()
-	load(df, engine, 'orders')	
+	for file_name, table_name in TABLES:
+		df = extract(file_name)
+		df = transform(df, table_name)
+		load(df, engine, table_name)
+	
+
+def check_file(file_name : str) -> None:
+	import pandas as pd
+	df = extract(file_name)
+	df.info()
 
 if __name__ == "__main__":
 	run_pipeline()
+	#check_file('olist_products_dataset.csv')
